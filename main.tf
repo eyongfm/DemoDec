f4c3eff7ed45c8ea3eeb6bb9b8d6b90e072f05b1
eyongfm <eyongfm@yahoo.fr>
2024-12-01 12:35:23 +0100

    Removed the google dns declaration

--- main.tf
+++ main.tf
@@ -2,11 +2,3 @@ resource "aws_instance" "app" {
   instance_type = "t2.micro"
   ami           = "ami-000b603b003994f09"
 }
-
-resource "google_dns_record_set" "a" {
-  name         = "demo.google-example.com"
-  managed_zone = "example-zone"
-  type         = "A"
-  ttl          = 300
-  rrdatas      = [aws_instance.app.public_ip]
-}
